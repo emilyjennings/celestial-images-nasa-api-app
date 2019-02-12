17b084303ea043c3773858fccbe96c1eefa418ac
github username <github email address>
2019-02-12 14:20:39 -0500

    playing around with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import IntroComponent from './IntroComponent.js'
+import CelestialComponent from './CelestialComponent.js'
 
 class App extends Component {
   render() {
+    // your code in the return statement below!
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <IntroComponent />
+        <CelestialComponent />
       </div>
     );
   }
